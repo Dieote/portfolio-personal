67329984b012df632bc47f8f780b0ee735bb2323
Dieote <gomezmdiego97@gmail.com>
2023-08-01 22:20:40 +0200

    Estilos Home imagen s efecto escritura

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
