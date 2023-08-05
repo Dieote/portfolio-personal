1ef4e7509946be0bf479f307b7b7b4cb1614b617
Dieote <gomezmdiego97@gmail.com>
2023-08-05 04:24:23 +0200

    Ampliacion botones services

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
 })
 export class ServicesComponent {
+  showModalWebDev = false;
+  showModalQA = false;
+  showModalRestaurant = false;
 
+  openModalWebDev(): void {
+    this.showModalWebDev = true;
+  }
+
+  closeModalWebDev(): void {
+    this.showModalWebDev = false;
+  }
+
+  openModalQA(): void {
+    this.showModalQA = true;
+  }
+
+  closeModalQA(): void {
+    this.showModalQA = false;
+  }
+
+  openModalRestaurant(): void {
+    this.showModalRestaurant = true;
+  }
+
+  closeModalRestaurant(): void {
+    this.showModalRestaurant = false;
+  }
 }
