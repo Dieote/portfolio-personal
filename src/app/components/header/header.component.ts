206a3a083b943955aaf797c819212f9cae355cb4
Dieote <gomezmdiego97@gmail.com>
2023-08-06 10:33:31 +0200

    Correcciones estilos

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  menuIconActive = false;
 
+  openIconActive() {
+    this.menuIconActive = !this.menuIconActive;
+  }
 }
